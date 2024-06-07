c8ea93e8f0a0d3876ddcdc57e13f42ac4995281f
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-06-07 12:55:39 +0300

    added gizlilik-politikası page

---
+++ src/app/gizlilik-politikasi/page.jsx
@@ -0,0 +1,152 @@
+import LeftMenu from "@/components/footerLinks/leftmenu";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Gizlilik politikası | Bilet 48",
+  description: "Gizlilik politikası description",
+};
+
+export default function Page() {
+  return (
+    <>
+      <div className="header-margin"></div>
+      <div className="container">
+        <div className="row">
+          <LeftMenu activeLink="gizlilik-politikasi" />
+          <div className="col-lg-9">
+            <div className="page-detail typography">
+              <div>
+                <p>
+                  <strong>GİZLİLİK</strong>
+                </p>
+                <p>
+                  Bilet 48, müşterilerine daha iyi hizmet verebilmek amacıyla
+                  bazı kişisel bilgilerinizi (isim, soyisim, telefon, yaş, ilgi
+                  alanlarınız, e-posta vb…) sizlerden talep etmektedir. Toplanan
+                  kişisel verileriniz 6698 sayılı Kişisel Verilerin Korunması
+                  Kanunu (“KVKK” olarak anılacaktır.) ve ilgili diğer mevzuatlar
+                  çerçevesinde işlenmektedir. Veriler KVKK ve ilgili
+                  mevzuatların öngördüğü işlenme amacına uygun süre ile
+                  saklanmaktadır. Hangi kişisel verilerinizin işlendiğine, hangi
+                  sürede ve amaçla saklandığına, toplanma yöntemi ve hukuki
+                  sebebine, haklarınıza ilişkin ayrıntılı bilgiye{" "}
+                  <Link href="/kvkk">
+                    “Kişisel Verilerin Korunması Politikası – Aydınlatma Metni”
+                  </Link>
+                  nden ulaşabilirsiniz.
+                </p>
+                <p>
+                  Bilet 48 Kullanıcılara ait bilgilerin gizliliğini
+                  sağlama-koruma hususunda son derece özen göstermektedir. Bilet
+                  48 veritabanında toplanan bu bilgiler, dönemsel kampanya
+                  çalışmaları, müşteri profillerine yönelik özel promosyon
+                  faaliyetlerinin kurgulanması ve istenmeyen e-postaların
+                  iletilmemesine yönelik müşteri “sınıflandırma” çalışmalarında
+                  sadece Bilet 48 tarafından kullanılmaktadır. Gizlilik
+                  Politikası herhangi başka bir kaynak veya yöntem kullanılmadan
+                  sadece Bilet 48 sitesi üzerinden elde edilen bilgiler için
+                  geçerlidir.
+                </p>
+                <p>
+                  Bilet 48, üyelik formlarından topladığı bilgileri, söz konusu
+                  üyenin haberi ya da aksi bir talimatı olmaksızın veya Kanunun
+                  öngördüğü haller dışında veya
+                  <Link href="/kvkk">
+                    “Kişisel Verilerin Korunması Politikası – Aydınlatma Metni”
+                  </Link>
+                  nde belirtilen haller dışında üçüncü şahıslarla kesinlikle
+                  paylaşmamakta, faaliyet dışı hiçbir nedenle ticari amaçla
+                  kullanmamakta ve de satmamaktadır.
+                </p>
+                <p>
+                  Bilet 48, e-posta adresleri ve üyelik formlarında istediği
+                  kişisel bilgilerin haricinde site kullanımı sırasında
+                  izlediği, ziyaretçi hareket ve tercihlerini analiz ederek
+                  yorumlamaktadır. Kişisel bilgiler içermeyen bu istatistiksel
+                  veriler, Bilet 48 müşterilerine daha özel ve etkin bir
+                  alışveriş deneyimi yaşatmak amacıyla Bilet 48&nbsp;iş
+                  ortakları ile paylaşılabilmektedir. İşbu Gizlilik Bildirimini
+                  kabul etmekle kişisel bilgilerinizin Bilet 48 iş ortakları ile
+                  paylaşılabileceğine onay vermiş olacaksınız.
+                </p>
+                <p>
+                  Sitemizde bulunan kullanıcıların kişisel bilgileri,
+                  şirketimizin iletişim kampanyaları (e-posta, SMS, bülten vs.)
+                  dâhilinde kullanıcıya bilgi vermek amacıyla kullanılabilir.
+                  Kullanıcılarımız bahsedilen tanıtımlardan haberdar olmak
+                  istemezlerse istedikleri zaman destek@bilet48.com.tr adresine
+                  e-mail göndererek bu aktiviteyi durdurabilirler. Yeni
+                  kampanyalar hakkında bilgi almak isteyen kullanıcılarımız
+                  www.bilet48.com.tr sitesi üzerinden haber bültenimize kayıt
+                  olabilirler veya daha önceki kayıtlarını silebilirler.
+                </p>
+                <p>
+                  Müşterinin sisteme girdiği tüm bilgilere sadece Müşteri
+                  ulaşabilmekte ve bu bilgileri sadece Müşteri
+                  değiştirebilmektedir. Bir başkasının bu bilgilere ulaşması ve
+                  bunları değiştirmesi mümkün değildir.
+                </p>
+                <p>
+                  Güncel fırsatlar ilgili gönderilen e-postalara ilave olarak,
+                  sitemiz hizmetlerinden faydalanarak satın aldığınız hizmetten
+                  nasıl faydalanacağınızı anlatan ve bu hizmetlerden
+                  faydalanmanızı sağlayacak bilgiler içeren mailleri de
+                  adresinize göndereceğiz. Hizmetlerden faydalanmanızı
+                  sağlayacak bu e-postalar başka herhangi bir içeriğe sahip
+                  olmayacak ve sadece ilgili hizmetlerle ilgili kodlar
+                  içerecektir. Bu sebeple, sitemize üye olurken verdiğiniz
+                  e-posta adresinin doğruluğunu da bu beyanımızı kabul etmekle
+                  teyit etmiş sayılırsınız.
+                </p>
+                <p>
+                  Kullanıcılarımızın satın alma işlemi sırasında bize vermiş
+                  olduğu bilgiler, kişilerin izni olmadıkça ve Kanunların
+                  öngördüğü haller bulunmadıkça ve
+                  <Link href="/kvkk">
+                    “Kişisel Verilerin Korunması Politikası – Aydınlatma Metni”
+                  </Link>
+                  nde belirtilen haller dışında 3.şahıs ya da kuruluşlarla
+                  paylaşılmaz. Bu bilgiler ödeme işlemleri ve fatura kesimi için
+                  istenmektedir.
+                </p>
+                <p>&nbsp;</p>
+                <p>
+                  Kredi kartı ile alışveriş yaptığınızda kredi kartı numarası,
+                  son kullanma tarihi, CVV2 ve benzeri bilgileri bankalarımız
+                  ile paylaşılmasını da kabul etmiş sayılırsınız. Aksi takdirde
+                  satın alma işlemini tamamlamanız mümkün değildir.
+                </p>
+                <p>
+                  Ödeme sayfasında istenen kredi kartı bilgileriniz, siteden
+                  alışveriş yapan siz değerli müşterilerimizin güvenliğini en
+                  üst seviyede tutmak amacıyla hiçbir şekilde Bilet48 veya ona
+                  hizmet veren şirketlerin sunucularında tutulmamaktadır. Bu
+                  şekilde ödemeye yönelik tüm işlemlerin Bilet48 ara yüzü
+                  üzerinden banka ve bilgisayarınız arasında gerçekleşmesi
+                  sağlanmaktadır.
+                </p>
+                <p>
+                  Sitemizde diğer web sitelerine link verebilmekteyiz. Gizlilik
+                  Politikamızdaki taahhütlerimiz yalnızca Sitemiz içerisinde
+                  geçerlidir. Diğer web siteleri bu kapsamda değildir.
+                  Sitemizden link ile ziyaret edilen diğer web sitelerinin kendi
+                  gizlilik ilkeleri, kullanım şartları geçerlidir. Bu sitelerde,
+                  bu siteler dolayısıyla uğranabilecek maddi/manevi hiçbir
+                  zarardan Bilet48 sorumlu değildir.
+                </p>
+                <p>
+                  Müşteri/Kullanıcı bilgileri, ancak resmi makamlarca usulü
+                  dairesinde bu bilgilerin talep edilmesi halinde ve
+                  yürürlükteki emredici mevzuat hükümleri gereğince resmi
+                  makamlara açıklama yapmak zorunda olduğu durumlarda resmi
+                  makamlara açıklanabilecektir.
+                </p>
+                <p>Bilgilerinize sunarız.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
