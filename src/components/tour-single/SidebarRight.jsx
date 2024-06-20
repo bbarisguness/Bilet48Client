adcf1a70dd666a89a297ea8cff80dd11a1d88749
barış güneş <bbarisguness@msn.com>
2024-06-20 15:46:02 +0300

    fix: some css changes

--- src/components/tour-single/SidebarRight.jsx
+++ src/components/tour-single/SidebarRight.jsx
@@ -6,10 +6,11 @@ const SidebarRight = ({ tour }) => {
       <div className="w-360 lg:w-full d-flex flex-column items-center">
         <div className="px-30 py-30 rounded-4 border-light bg-white shadow-4">
           <div className="text-14 text-light-1">
-            From{" "}
-            <span className="text-20 fw-500 text-dark-1 ml-5">
-              US${tour?.price}
+          <span className="text-20 fw-500 text-dark-1 ml-5">
+          {tour?.price} TL'den
             </span>
+        
+          {" "}Başlayan Fiyatlarla
           </div>
           {/* End div */}
 
@@ -22,7 +23,7 @@ const SidebarRight = ({ tour }) => {
               <i className="icon-heart text-16 text-green-2" />
             </div>
             <div className="text-14 lh-16 ml-10">
-              94% of travelers recommend this experience
+              Biletler tükenmeden hemen yerini ayırt..
             </div>
           </div>
         </div>
@@ -30,8 +31,6 @@ const SidebarRight = ({ tour }) => {
 
         <div className="px-30">
           <div className="text-14 text-light-1 mt-30">
-            Not sure? You can cancel this reservation up to 24 hours in advance
-            for a full refund.
           </div>
         </div>
         {/* End div */}
--- src/components/tour-single/filter-box/index.jsx
+++ src/components/tour-single/filter-box/index.jsx
@@ -5,22 +5,20 @@ import Link from "next/link";
 const index = () => {
   return (
     <>
-      <div className="col-12">
+      
+      {/* <div className="col-12">
         <div className="searchMenu-date px-20 py-10 border-light rounded-4 -right js-form-dd js-calendar">
           <div>
             <h4 className="text-15 fw-500 ls-2 lh-16">Date</h4>
             <DateSearch />
           </div>
         </div>
-        {/* End check-in-out */}
       </div>
-      {/* End .col-12 */}
-
+      
       <div className="col-12">
         <GuestSearch />
-        {/* End guest */}
-      </div>
-      {/* End .col-12 */}
+      </div> */}
+      
 
       <div className="col-12">
         <Link
