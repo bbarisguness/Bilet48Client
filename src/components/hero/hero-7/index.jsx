060d384f1fcdbb549cd4bd5ba3e6072a95c449de
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-06-24 15:08:05 +0300

    home slider photos changed

--- src/components/hero/hero-7/index.jsx
+++ src/components/hero/hero-7/index.jsx
@@ -1,5 +1,4 @@
-
-'use client'
+"use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
@@ -17,12 +16,44 @@ const index = () => {
               prevEl: ".hero7-prev-active",
             }}
           >
-            <SwiperSlide style={{height: "350px"}}>
-              <div className="row justify-center text-center" style={{height: "100%"}}>
-                <div className="col-auto" style={{height: "100%"}}>
-                  <div className="masthead__content" style={{height: "100%"}}>
+            <SwiperSlide style={{ height: "350px" }}>
+              <div
+                className="row justify-center text-center"
+                style={{ height: "100%" }}
+              >
+                <div className="col-auto" style={{ height: "100%" }}>
+                  <div className="masthead__content" style={{ height: "100%" }}>
                     <div className="masthead__bg">
-                      <img src="/img/masthead/7/bg.png" alt="image" />
+                      <img src="/img/masthead/homeSlider1.jpg" alt="image" />
+                    </div>
+                    <div
+                      className="text-white"
+                      data-aos="fade-up"
+                      data-aos-delay="100"
+                    >
+                      Discover amzaing places at exclusive deals
+                    </div>
+                    <h1
+                      className="text-60 lg:text-40 md:text-30 text-white"
+                      data-aos="fade-up"
+                      data-aos-delay="300"
+                    >
+                      Unique Houses Are Waiting
+                      <br className="lg:d-none" /> For You
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide style={{ height: "350px" }}>
+              <div
+                className="row justify-center text-center"
+                style={{ height: "100%" }}
+              >
+                <div className="col-auto" style={{ height: "100%" }}>
+                  <div className="masthead__content" style={{ height: "100%" }}>
+                    <div className="masthead__bg">
+                      <img src="/img/masthead/homeSlider2.jpg" alt="image" />
                     </div>
                     <div
                       className="text-white"
